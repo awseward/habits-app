fb0a0c104909158cd4149466eec2bce79794ba1e
Andrew Seward <awswrd@gmail.com>
2018-10-20 00:10:44 -0700

    feat: fake targets for heroku container deploy

--- build.fsx
+++ build.fsx
@@ -12,12 +12,30 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open System
 
 let TODO _ : unit = failwith "TODO"
+
+let appPath = Path.getFullName "./src/HabitsApp"
+let deployDir = Path.getFullName "./deploy"
+
+let runDotNet cmd workingDir =
+  let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+  if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+let runTool cmd args workingDir =
+  let result =
+    Process.execSimple (fun info ->
+      { info with
+          FileName = cmd
+          WorkingDirectory = workingDir
+          Arguments = args })
+      TimeSpan.MaxValue
+  if result <> 0 then failwithf "'%s %s' failed" cmd args
 
 Target.create "Clean" (fun _ ->
   !! "src/**/bin"
   ++ "src/**/obj"
+  ++ deployDir
   |> Shell.cleanDirs
 )
 
@@ -28,11 +46,20 @@ Target.create "Build" (fun _ ->
 
 Target.create "Run" TODO
 
-Target.create "Bundle" TODO
+Target.create "Bundle" (fun _ ->
+  let appDeployDir = Path.combine deployDir "HabitsApp"
 
-Target.create "Heroku:Container:Push" TODO
+  runDotNet (sprintf "publish -c Release -o \"%s\"" appDeployDir) appPath
+)
 
-Target.create "Heroku:Container:Release" TODO
+Target.create "Heroku:Container:Push" (fun _ ->
+  runTool "heroku" "container:push web --recursive" "."
+)
+
+Target.create "Heroku:Container:Release" (fun _ ->
+  runTool "heroku" "container:release web" "."
+  runTool "heroku" "open" "."
+)
 
 "Clean"
   ==> "Build"
