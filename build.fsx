88ca9d511aabdf1fb8c70b1d1b33aaef6f9eb7de
Andrew Seward <awswrd@gmail.com>
2018-10-19 21:05:40 -0700

    wip: Think this should work but FAKE is now broken
    Steps to reproduce:
    
    1. Check out master (currently @ faea19e1132e4cf9674e063a195fbd08074d4a02)
    2. `git clean -xdf`
    3. `fake build --list`
    4. Check out this commit
    5. `fake build --list` (should work just fine)
    6. `git clean -xdf`
    7. `fake build --list` (should now be broken)
    
    Need to investigate what has changed between the branches that is
    causing it to break.
    
    Potential things to examine: Difference between `.fake/` directories in
    the two states.

--- build.fsx
+++ build.fsx
@@ -2,8 +2,8 @@
 #load "./.fake/build.fsx/intellisense.fsx"
 
 #if !FAKE
-#r "netstandard"
-#r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
+  #r "netstandard"
+  #r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
 #endif
 
 open Fake.Core
