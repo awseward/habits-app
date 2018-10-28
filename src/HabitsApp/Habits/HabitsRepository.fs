d05c66d5fd460e1b21e2dd22754df1ca7293a040
Andrew Seward <awswrd@gmail.com>
2018-10-28 00:05:21 -0700

    fix: Postgresql-ify habits code

--- src/HabitsApp/Habits/HabitsRepository.fs
+++ src/HabitsApp/Habits/HabitsRepository.fs
@@ -1,44 +1,39 @@
 namespace Habits
 
+
 open Database
-open Microsoft.Data.Sqlite
-open System.Threading.Tasks
 open FSharp.Control.Tasks.ContextInsensitive
+open Npgsql
+open System.Threading.Tasks
 
 module Database =
   let getAll connectionString : Task<Result<Habit seq, exn>> =
     task {
-      use connection = new SqliteConnection(connectionString)
+      use connection = new NpgsqlConnection(connectionString)
       return! query connection "SELECT id, name FROM Habits" None
     }
 
   let getById connectionString id : Task<Result<Habit option, exn>> =
     task {
-      use connection = new SqliteConnection(connectionString)
+      use connection = new NpgsqlConnection(connectionString)
       return! querySingle connection "SELECT id, name FROM Habits WHERE id=@id" (Some <| dict ["id" => id])
     }
 
   let update connectionString v : Task<Result<int,exn>> =
     task {
-      use connection = new SqliteConnection(connectionString)
+      use connection = new NpgsqlConnection(connectionString)
       return! execute connection "UPDATE Habits SET id = @id, name = @name WHERE id=@id" v
     }
 
   let insert connectionString v : Task<Result<int,exn>> =
     task {
-      use connection = new SqliteConnection(connectionString)
-      let query = @"
-INSERT INTO Habits(id, name)
-  VALUES (
-    (SELECT IFNULL(MAX(id), 0) + 1 FROM Habits),
-    @name
-  )"
+      use connection = new NpgsqlConnection(connectionString)
+      let query = "INSERT INTO Habits(name) VALUES (@name)"
       return! execute connection query v
     }
 
   let delete connectionString id : Task<Result<int,exn>> =
     task {
-      use connection = new SqliteConnection(connectionString)
+      use connection = new NpgsqlConnection(connectionString)
       return! execute connection "DELETE FROM Habits WHERE id=@id" (dict ["id" => id])
     }
-
