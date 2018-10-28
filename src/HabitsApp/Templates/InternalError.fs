751a342e2559fc14d78e480a5eb9ccbf056da22f
Andrew Seward <awswrd@gmail.com>
2018-10-28 12:09:50 -0700

    fix: Minor improvements to error pages
    * Crude implimentation of not showing error details on prod
    * Some adjustment of wording

--- src/HabitsApp/Templates/InternalError.fs
+++ src/HabitsApp/Templates/InternalError.fs
@@ -2,18 +2,20 @@ module InternalError
 
 open System
 open Giraffe.GiraffeViewEngine
-let layout (ex: Exception) =
+
+let layout isProduction (ex: Exception)  =
     html [_class "has-navbar-fixed-top"] [
         head [] [
             meta [_charset "utf-8"]
             meta [_name "viewport"; _content "width=device-width, initial-scale=1" ]
-            title [] [encodedText "SaturnSample - Error #500"]
+            title [] [encodedText "Error 500"]
         ]
         body [] [
-           h1 [] [rawText "ERROR #500"]
-           h3 [] [rawText ex.Message]
-           h4 [] [rawText ex.Source]
-           p [] [rawText ex.StackTrace]
-           a [_href "/" ] [rawText "Go back to home page"]
+           yield h1 [] [rawText "Internal Server Error"]
+           if (not isProduction) then
+               yield h3 [] [rawText ex.Message]
+               yield h4 [] [rawText ex.Source]
+               yield p [] [rawText ex.StackTrace]
+           yield a [_href "/" ] [rawText "Go back to home page"]
         ]
     ]
