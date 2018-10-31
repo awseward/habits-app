e74f491988ce766753b30686a2a2e8f2eaa9c59c
Andrew Seward <awswrd@gmail.com>
2018-10-31 00:37:56 -0700

    fix: Add turbolinks.js, handle turbolinks:load
    Turns out there was a bit more to it. Definitely not much, though.

--- src/HabitsApp/static/app.js
+++ src/HabitsApp/static/app.js
@@ -1,27 +1,3 @@
-function addNavbarBurger () {
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-}
-
 function addDeleteButtons () {
   var $deleteButtons = Array.prototype.slice.call(document.querySelectorAll('.is-delete'), 0);
   if ($deleteButtons.length > 0) {
@@ -51,8 +27,10 @@ function addOnclickToSetLastDoneAsNowButtonIfPresent() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  addNavbarBurger();
+function setUpThePage() {
   addDeleteButtons();
   addOnclickToSetLastDoneAsNowButtonIfPresent();
-});
+}
+
+document.addEventListener('DOMContentLoaded', setUpThePage);
+document.addEventListener('turbolinks:load', setUpThePage);
